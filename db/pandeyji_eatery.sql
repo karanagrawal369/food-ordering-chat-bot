91042e202e2c397b990b60ed3e5e02e4ea55b3c9
karanagrawal369 <karanagrawal2002@gmail.com>
2025-02-06 21:18:34 +0530

    modifcation

--- db/pandeyji_eatery.sql
+++ db/pandeyji_eatery.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `pandeyji_eatery` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
 USE `pandeyji_eatery`;
 -- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
 --
